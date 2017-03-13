0c74ac1b7cf7153e0c08c1fbb15357d31cdd0d50
Ivanboh <gombajz@gmail.com>
2017-03-13 11:21:30 +0100

    zmeny

--- Norsko/feedback.php
+++ Norsko/feedback.php
@@ -1,39 +1,50 @@
 <?php
-  //function injects_safe($string) {
-  //  return mysql_real_escape_string($string);
-  //}
+include 'header.php';
 
-  //function results_assoc($result) {
-  //  $tmp = Array();
-  //  while($row = mysql_fetch_assoc($result)) {
-  //    array_push($tmp, $row);
-  //  }
-  //  return $tmp;
-  //}
-  //riesil som z domu
- $servername = "localhost";
-$username = "root";
-$password = "";
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $meno = test_input($_POST["meno"]);
+    $email = test_input($_POST["email"]);
+    $text = test_input($_POST["text"]);
 
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
+    $sql = "INSERT INTO odkazy (meno, email, text) VALUES ('$meno', '$email', '$text')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "správa bola odoslanáˇ";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+
 }
 
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
 }
 
-$conn->close();
-  }
+
 ?>
-<?php include 'header.php'; ?>
 
 
-         
+<div class="wrap">
+    <form class="form-horizontal" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+        <div class="form-group">
+            <label for="meno">Meno</label>
+            <input type="text" class="form-control" name="meno" placeholder="zadaj meno" />
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="email" class="form-control" name="email" placeholder="zadaj email" />
+        </div>
+        <textarea class="form-control" rows="3" name="text"></textarea>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-default">odoslať</button>
+            </div>
+        </div>
+
+    </form>
+</div>
+<?php include 'footer.php'; ?>
--- Norsko/header.php
+++ Norsko/header.php
@@ -1,7 +1,12 @@
 <!DOCTYPE html>
+
+<?php include 'Conect.php';
+?>
 <html lang="sk">
 <head>
-  <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Homepage</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
@@ -16,7 +21,7 @@
         <a href="stranka1.php">NÓRSKO</a>
         <a href="stranka2.php">Príprava</a>
         <a href="stranka3.php">Cesta</a>
-        <a href="feedback.php"></a>
+        <a href="feedback.php">formular</a>
       </nav>
     </div>
   </header>
